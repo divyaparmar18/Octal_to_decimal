cfc1aef83caef9aa66aa34d7f1f6d4c6590973de
divyaparmar18 <divya18@navgurukul.org>
2019-05-02 22:05:01 +0530

    defining variables

--- octal_decimal.java
+++ octal_decimal.java
@@ -3,8 +3,10 @@ public class octal_decimal {
     public static void main(String[]args){
         Scanner obj = new Scanner(System.in);
         System.out.println("enter a nmber to be converted");
-        int userNmber = obj.nextInt();
-        System.out.println(userNmber);
+        int octalNumber = obj.nextInt();
+        System.out.println(octalNumber);
+        int i=0,decimalNumber=0,lastDigit,exponent=8;
+
     }
 
 
