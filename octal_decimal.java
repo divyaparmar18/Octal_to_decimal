c28765c8c9855230742b1032aa42d90970bcdae0
divyaparmar18 <divya18@navgurukul.org>
2019-05-02 21:54:07 +0530

    taking userinpt

---
+++ octal_decimal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class octal_decimal {
+    public static void main(String[]args){
+        Scanner obj = new Scanner(System.in);
+        System.out.println("enter a nmber to be converted");
+        int userNmber = obj.nextInt();
+        System.out.println(userNmber);
+    }
+
+
+}
