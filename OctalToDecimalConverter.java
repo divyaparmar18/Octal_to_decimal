d544ffe2e45acac63f901666e44679f62920403b
divyaparmar18 <divya18@navgurukul.org>
2019-05-05 10:38:12 +0530

    adding the exponent of lastDigits

--- OctalToDecimalConverter.java
+++ OctalToDecimalConverter.java
@@ -1,30 +1,15 @@
 public class OctalToDecimalConverter {
     public static void main(String[]args) {
         String octalNumber = args[0];
-        boolean notDecimal = true;
-        int i=0,decimalNumber=0,exponent=8,lastDigit;
+        boolean notOctal = true;
+        int i = 0, decimalNumber = 0, exponent = 8, lastDigit;
         String[] number = octalNumber.split("");
-        for(int len = number.length;len>0;len--){
-            lastDigit = Integer.parseInt(number[len-1]);
-            if (lastDigit > 7){
-                notDecimal = false;
+        for (int len = number.length; len > 0; len--) {
+            lastDigit = Integer.parseInt(number[len - 1]);
+            if (lastDigit > 7) {
+                notOctal = false;
                 System.out.println("Not valid, the number is not an octal number");
-            }
+            } else decimalNumber += lastDigit * ((int) Math.pow(exponent, i));
         }
-//        boolean notDecimal = true;
-//        if (notDecimal){
-//            System.out.println(decimalNumber);
-//        }
-//
-//    }
-//
-//    private static int getQuotient(int octalNumber) {
-//        return octalNumber / 10;
-//    }
-//
-//    private static int getLastDigit(int octalNumber) {
-//        return octalNumber % 10;
-  }
-//
-//
+    }
 }
