323cac485ae2932ec9ca285d6cb211c659055d81
divyaparmar18 <divya18@navgurukul.org>
2019-05-05 13:21:22 +0530

    Fix. Go back to integer implementation

--- OctalToDecimalConverter.java
+++ OctalToDecimalConverter.java
@@ -1,19 +1,34 @@
 public class OctalToDecimalConverter {
-    public static void main(String[]args) {
-        String octalNumber = args[0];
-        boolean ifOctal = true;
-        int i = 0, decimalNumber = 0, exponent = 8, lastDigit;
-        String[] number = octalNumber.split("");
-        for (int len = number.length; len > 0; len--) {
-            lastDigit = Integer.parseInt(number[len - 1]);
-            if (lastDigit > 7) {
-                ifOctal = false;
-                System.out.println("Not valid, the number is not an octal number");
-            } else decimalNumber += lastDigit * ((int) Math.pow(exponent, i));
-            i++;
+    public static void main(String[]args){
+        int octalNumber = Integer.parseInt(args[0]);
+        int i=0,decimalNumber=0,lastDigit,exponent=8;
+        boolean notDecimal = true;
+        while(octalNumber!=0) {
+            lastDigit = getLastDigit(octalNumber);
+            if(lastDigit > 7) {
+                notDecimal = false;
+                System.out.println("your input is not an octal number");
+                break;
+            }
+            else{
+                octalNumber= getQuotient(octalNumber);
+                decimalNumber+=lastDigit * ((int)(Math.pow(exponent,i)));
+                i++;
+            }
         }
-        if (ifOctal) {
-            System.out.println("The decimal number of the number you have given is" + decimalNumber);
+        if (notDecimal){
+            System.out.println("The decimal number of the number you have given is "+decimalNumber);
         }
+
     }
+
+    private static int getQuotient(int octalNumber) {
+        return octalNumber / 10;
+    }
+
+    private static int getLastDigit(int octalNumber) {
+        return octalNumber % 10;
+    }
+
+
 }
