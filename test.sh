407fe209e8fb0f59b7afbb84d753eac6e85741e6
divyaparmar18 <divya18@navgurukul.org>
2019-05-05 14:02:08 +0530

    add .testcase for number 9

--- test.sh
+++ test.sh
@@ -3,4 +3,8 @@ java OctalToDecimalConverter 10 > actual
 diff actual ExpectedOutput
 java OctalToDecimalConverter 0 > actual
 diff actual Expect
+java OctalToDecimalConverter 9 > actual
+diff actual Expected
+
+
 
