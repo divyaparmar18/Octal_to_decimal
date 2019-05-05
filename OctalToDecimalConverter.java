9291256be77ac58536668db303da56173fca1260
divyaparmar18 <divya18@navgurukul.org>
2019-05-05 10:21:48 +0530

    add edge cases

--- OctalToDecimalConverter.java
+++ OctalToDecimalConverter.java
@@ -1,26 +1,17 @@
 public class OctalToDecimalConverter {
     public static void main(String[]args) {
         String octalNumber = args[0];
+        boolean notDecimal = true;
+        int i=0,decimalNumber=0,exponent=8,lastDigit;
         String[] number = octalNumber.split("");
         for(int len = number.length;len>0;len--){
-            System.out.println(number[len-1]);
-
+            lastDigit = Integer.parseInt(number[len-1]);
+            if (lastDigit > 7){
+                notDecimal = false;
+                System.out.println("Not valid, the number is not an octal number");
+            }
         }
-        int i=0,decimalNumber=0,exponent=8;
 //        boolean notDecimal = true;
-//        while(octalNumber!=0) {
-//            lastDigit = getLastDigit(octalNumber);
-//            if(lastDigit > 7) {
-//                notDecimal = false;
-//                System.out.println("your input is not an octal number");
-//                break;
-//            }
-//            else{
-//                octalNumber= getQuotient(octalNumber);
-//                decimalNumber+=lastDigit * ((int)(Math.pow(exponent,i)));
-//                i++;
-//            }
-//        }
 //        if (notDecimal){
 //            System.out.println(decimalNumber);
 //        }
