5257849e7d40f53930322e5c295782787cbbd982
divyaparmar18 <divya18@navgurukul.org>
2019-05-05 14:03:19 +0530

    add .testcase for number 01

--- test.sh
+++ test.sh
@@ -5,6 +5,10 @@ java OctalToDecimalConverter 0 > actual
 diff actual Expect
 java OctalToDecimalConverter 9 > actual
 diff actual Expected
+java OctalToDecimalConverter 01 > actual
+diff actual Expected1
+
+
 
 
 
