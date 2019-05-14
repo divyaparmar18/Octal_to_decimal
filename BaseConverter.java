04c9d88b7b967dc41303901aa905af539ccde4e1
divya <divya18@navgurukul.org>
2019-05-14 11:56:22 +0530

    refactor . isNotOctal to isNotValid

--- BaseConverter.java
+++ BaseConverter.java
@@ -2,55 +2,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BaseConverter {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         String octal = args[2];
         int lengthOfNum = octal.length();
-        if(isNotoctal(octal)){
+        if (isNotValid(octal)) {
             System.out.println("your input is not an octal number");
             return;
         }
         int base = Integer.parseInt(args[1]);
-        int decimal = getDotProduct(getDigits(octal),getPowers(base,lengthOfNum));
+        int decimal = getDotProduct(getDigits(octal), getPowers(base, lengthOfNum));
         System.out.println(decimal);
     }
 
 
     private static List<Integer> getDigits(String number) {
-        String [] NumOfString = number.split("");
+        String[] NumOfString = number.split("");
         List<Integer> splitOfDigits = new ArrayList<>();
-        for(int i = NumOfString.length;i>0;i--){
-            splitOfDigits.add(Integer.valueOf(NumOfString[i-1]));
+        for (int i = NumOfString.length; i > 0; i--) {
+            splitOfDigits.add(Integer.valueOf(NumOfString[i - 1]));
         }
         return (splitOfDigits);
     }
 
     private static List<Integer> getPowers(int base, int times) {
         List<Integer> listOfPowers = new ArrayList<Integer>();
-        for (int i = 0; i <times ; i++) {
-         int Power =((int)(Math.pow(base,i)));
-         listOfPowers.add(Power);
+        for (int i = 0; i < times; i++) {
+            int Power = ((int) (Math.pow(base, i)));
+            listOfPowers.add(Power);
         }
         return listOfPowers;
     }
 
-    private static int getDotProduct(List<Integer> digitsOfNumber, List<Integer> powersOfEight){
+    private static int getDotProduct(List<Integer> digitsOfNumber, List<Integer> powersOfEight) {
         int product = 0;
-        for (int i = 0; i <digitsOfNumber.size() ; i++) {
+        for (int i = 0; i < digitsOfNumber.size(); i++) {
             product += digitsOfNumber.get(i) * powersOfEight.get(i);
         }
         return product;
     }
 
-    private static boolean isNotoctal(String octal) {
+    private static boolean isNotValid(String octal) {
         return octal.contains("8") || (octal.contains("9"));
     }
 
-    private static int getQuotient(int octalNumber) {
-        return octalNumber / 10;
-    }
-
-    private static int getLastDigit(int octalNumber) {
-        return octalNumber % 10;
-    }
 }
-
--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
-javac OctalToDecimalConverter.java
-java OctalToDecimalConverter --base 8 10 > actual
+javac BaseConverter.java
+java BaseConverter --base 8 10 > actual
 diff actual Expected8
-java OctalToDecimalConverter --base 2 100 > actual
+java BaseConverter --base 2 100 > actual
 diff actual Expectfor2
-java OctalToDecimalConverter --base 6 120 > actual
+java BaseConverter --base 6 120 > actual
 diff actual Expectfor6
-java OctalToDecimalConverter --base 5 134 > actual
+java BaseConverter --base 5 134 > actual
 diff actual Expectfor5
 
 
