3546919fb0992acd366c5a954d876f044fed54a5
divya <divya18@navgurukul.org>
2019-05-14 12:47:04 +0530

    refactor . expected output files name

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 javac BaseConverter.java
 java BaseConverter --base 8 10 > actual
-diff actual Expected8
+diff actual ExpectedFor10InBase8
 java BaseConverter --base 2 100 > actual
-diff actual Expectfor2
+diff actual ExpectFor100InBase2
 java BaseConverter --base 6 120 > actual
-diff actual Expectfor6
+diff actual ExpectFor120InBase6
 java BaseConverter --base 5 134 > actual
-diff actual Expectfor5
+diff actual ExpectFor134InBase5
 java BaseConverter --base 2 45 > actual
-diff actual ExpectInvalid
+diff actual ExpectInvalidFor45InBase2
 
 
 
